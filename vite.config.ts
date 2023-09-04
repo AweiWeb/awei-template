dfe32f43f2d6cf895cd01d97751bd8343807aead
王超伟 <>
2023-09-04 17:37:45 +0800

    分块打包

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, ConfigEnv, loadEnv } from 'vite';
 import path from 'path';
 import { parseEnv } from './vite/utils';
 import setupPlugin from './vite/plugins';
-import { visualizer } from "rollup-plugin-visualizer";
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [vue()],
@@ -20,12 +20,30 @@ export default ({ command, mode }: ConfigEnv) => {
   const env = parseEnv(loadEnv(mode, root));
   console.log(env);
   return {
+    // 可视化打包的插件
     plugins: [...setupPlugin(isBuild, env), visualizer()],
     resolve: {
       alias: {
         // 配置引入别名
         '@': path.resolve(__dirname, 'src'),
         '#': path.resolve(__dirname, 'types'),
+      },
+    },
+    build: {
+      rollupOptions: {
+        emptyOutDir: true,
+        output: {
+          // 分块打包
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString();
+            }
+          },
+        },
       },
     },
   };
